f322061926e6630559a1ed2d9ce1d8ce09318433
Yanik Ceulemans <yanik.ceulemans@gmail.com>
2021-08-30 20:09:05 +0200

    Added functioning tests

--- test/saver.test.ts
+++ test/saver.test.ts
@@ -1,7 +1,22 @@
+import axios from "axios";
 import { saveFile } from "../src/saver";
 
-describe('saveFile', () => {
-  it('should work', () => {
-    expect(1).toEqual(2);
-  })
+describe("saveFile", () => {
+  it("should work", async () => {
+    jest.spyOn(axios, "post").mockResolvedValue({ status: 200 });
+    jest.spyOn(console, 'log').mockReturnValue(undefined);
+
+    await expect(
+      saveFile("test/test-file.txt", Buffer.from("test contents"))
+    ).resolves.toEqual(undefined);
+  });
+
+  it("should throw an error when saving failed", async () => {
+    jest.spyOn(axios, "post").mockResolvedValue({ status: 500 });
+    jest.spyOn(console, 'log').mockReturnValue(undefined);
+
+    await expect(
+      saveFile("test/test-file.txt", Buffer.from("test contents"))
+    ).rejects.toThrow(/Failed to save file/);
+  });
 });
